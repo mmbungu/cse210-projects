9a949b9d725a914d290e2078fe98131a862475c2
intellsyst <intellsystrdc@gmail.com>
2026-07-04 09:27:48 +0100

    Complete exercice 2

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name: ");
+        
+        string name = Console.ReadLine();
+
+        Console.Write("What is your last name: ");
+
+        string lastname = Console.ReadLine();
+
+
+        Console.WriteLine($"Your name is {name} {lastname}");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,71 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your pourcentage ? ");
+        int percentage = int.Parse(Console.ReadLine());
+        string grade;
+
+        Console.WriteLine($"Your percentage is {percentage}%");
+
+        if(percentage >= 90)
+        {
+
+           grade = "A";
+           if (percentage < 93)
+           {
+                grade += "-";
+           }
+        }
+        else if (percentage >= 80)
+        {
+            grade = "B";
+            if (percentage < 83)
+            {
+                grade += "-";
+            }
+            else if (percentage >= 87)
+            {
+                grade += "+";
+            }
+        }
+        else if (percentage >= 70)
+        {
+            grade = "C";
+            if (percentage < 73)
+            {
+                grade += "-";
+            }
+            else if (percentage >= 77)
+            {
+                grade += "+";
+            }
+        }
+        else if (percentage >= 60)
+        {
+            grade = "D";
+            if (percentage < 63)
+            {
+                grade += "-";
+            }
+            else if (percentage >= 67)
+            {
+                grade += "+";
+            }
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is {grade}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass the class. Better luck next time!");
+        }
     }
 }
