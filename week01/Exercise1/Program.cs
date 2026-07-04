456221331be530687ad4646c4bddfb60ec391359
intellsyst <intellsystrdc@gmail.com>
2026-07-04 22:34:44 +0100

    Fix Exercice 1 according to requirement

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,14 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name: ");
-        
         string name = Console.ReadLine();
 
         Console.Write("What is your last name: ");
-
         string lastname = Console.ReadLine();
 
-
-        Console.WriteLine($"Your name is {name} {lastname}");
+        Console.WriteLine($"Your name is {lastname}, {name} {lastname}.");
     }
 }
