7fc41daeda6353121867b2ffbad6c0f51bd64241
intellsyst <intellsystrdc@gmail.com>
2026-07-04 12:28:30 +0100

    Complete exercice 3 & 4

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+
+        int magicNumber = randomGenerator.Next(1, 11);
+        int inputNumber;
+        int guessCount = 0;
+
+        while(true)
+        {
+            Console.Write("What's your magic number: ");
+            inputNumber = int.Parse(Console.ReadLine());
+            guessCount++;
+
+            if (inputNumber > magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (inputNumber < magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine($"You found it ! the magic number was {magicNumber} and you found it after {guessCount} times");
+                
+                Console.Write("Would you play again (yes/no) ? ");
+                string retry = Console.ReadLine();
+
+                if (retry == "no")
+                {
+                    break;
+                }
+
+                magicNumber = randomGenerator.Next(1, 11);
+                guessCount = 0;
+            }
+        }
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,39 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<float> numbers = new List<float>();
+
+        while(true)
+        {
+            Console.Write("Enter number: ");
+            float number = float.Parse(Console.ReadLine());
+
+            if (number == 0)
+            {
+                break;
+            }
+
+            numbers.Add(number);
+        }
+
+        Console.WriteLine($"The sum is {numbers.Sum()}");
+        Console.WriteLine($"The average is {numbers.Sum() / numbers.Count}");
+
+        float largest = 0;
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] > largest)
+            {
+                largest = numbers[i];
+            }
+        }
+
+        Console.WriteLine($"The largest number is {largest}");
+
+        
     }
 }
