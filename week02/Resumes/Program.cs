aa6ca79bb743b5e3d2148b3dc62cdeabf19917aa
intellsyst <intellsystrdc@gmail.com>
2026-07-04 23:30:26 +0100

    Complete Week2 Exercices 1

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job = new Job();
+        job._company = "Microsoft";
+        job._jobTitle = "Software Engineer";
+        job._startYear = 2018;
+        job._endYear = 2020;
+
+        Job job1 = new Job();
+        job1._company = "Apple";
+        job1._jobTitle = "Product Manager";
+        job1._startYear = 2020;
+        job1._endYear = 2022;
+
+        Resume resume = new Resume();
+        resume._name = "Goms";
+
+        resume._jobs.Add(job);
+        resume._jobs.Add(job1);
+
+        resume.Display();
+
     }
 }
