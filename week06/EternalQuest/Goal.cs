f0ba5d266ec3ed2d373f269286302ee9becdd9fb
intellsyst <intellsystrdc@gmail.com>
2026-08-09 04:27:00 +0100

    Add Goal Base class

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,27 @@
+using System;
+
+public abstract class Goal
+{
+    protected string _shortName;
+    protected string _description;
+    protected string _points;
+
+    public Goal(string name, string description, string points)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+    }
+
+    public abstract void RecordEvent();
+
+    public abstract bool IsComplete();
+
+    public virtual string GetDetailsString()
+    {
+        string checkbox = IsComplete() ? "[X]" : "[ ]";
+        return $"{checkbox} {_shortName} ({_description})";
+    }
+
+    public abstract string GetStringRepresentation();
+}
